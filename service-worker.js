a65896985cd4acf37e7fc6ddccbad221bc1bc216
Thai Le <thai.le@vice.com>
2016-11-12 16:53:49 -0600

    finishing touches

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var cacheName = 'tolydia-1';
+var cacheName = 'tolydia-1.1';
 var filesToCache = [
     'index.html',
     'sunset.mp4'
