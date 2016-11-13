8f53051fd81237fa021c4c047ffdc2503d61b46a
Thai Le <thai.le@vice.com>
2016-11-12 18:22:33 -0600

    add music

--- service-worker.js
+++ service-worker.js
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var cacheName = 'tolydia-1.1';
+var cacheName = 'tolydia-1.2';
 var filesToCache = [
     'index.html',
-    'sunset.mp4'
+    'sunset.mp4',
+    'song-cut.mp3'
 ];
 
 self.addEventListener('install', function(e) {
